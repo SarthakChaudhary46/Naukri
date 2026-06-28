39d3be48914f9158266ecaabd299a138dbbd555e
SarthakChaudhary46 <chaudharysarthak46@gmail.com>
2026-06-28 10:11:10 +0000

    Naukri Changes

--- Main.py
+++ Main.py
@@ -1,19 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import os
 
-USERNAME = os.environ["USERNAME"]
-PASSWORD = os.environ["PASSWORD"]
+USERNAME = os.getenv("USERNAME") or os.getenv("NAUKRI_USERNAME")
+PASSWORD = os.getenv("PASSWORD") or os.getenv("NAUKRI_PASSWORD")
+
+if not USERNAME or not PASSWORD:
+    raise RuntimeError("USERNAME and PASSWORD must be provided via environment variables")
 
 # ---------------- Chrome ----------------
 options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 options.add_argument("--disable-notifications")
 
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 try:
     # ---------------- Login ----------------
